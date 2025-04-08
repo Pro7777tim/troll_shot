714006f5b3ff852d959c5c724dd75fef18051fd3
Pro7777tim <khtim1226@gmail.com>
2025-04-08 15:16:15 +0300

    edit menu

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-#TrollShot
+# TrollShot
+
+## About
+
+Browser game similar to Buckshot Roulette.
+<br/>
+In the game you have to play Russian roulette with a troll, using various objects.
+<br/>
+The game has complex mechanics (luck, money, achievements, different modes).
+<br/>There are many jokes. The game is made very high quality.
+<br/>
+The animations are smooth, the textures and sounds are high quality.
+<br/>
+Management is not complicated. Have a luck game! ;>
+<br/>
+P.s don't anger the troll!
+
+## Run
+
+You can run it locally (run python server or open index.html), or you can use a public url.
+
+## License
+
+The game itself uses some media from the Internet, so we do not forbid you from taking any elements of the game!
--- src/js/classic_script.js
+++ src/js/classic_script.js
@@ -1,15 +1,91 @@
-const follower = document.getElementById("cursor");
-let posX = window.innerWidth / 2;
-let posY = window.innerHeight / 2;
-
-document.addEventListener("mousemove", (e) => {
-    posX = e.clientX;
-    posY = e.clientY;
-});
-
+//add function and value
+function random(num) {
+    return Math.floor(Math.random() * num) + 1;
+}
 function animate() {
-    follower.style.transform = `translate(${posX}px, ${posY}px)`;
+    follower.style.left = percentX + "%";
+    follower.style.top = percentY + "%";
     requestAnimationFrame(animate);
 }
+function link() {
+    let change = random(6);
+    if (change == 1) {
+        window.open("https://www.youtube.com/watch?v=j-iheFkstFQ", "_blank");
+    } else if (change == 2) {
+        window.open("https://www.youtube.com/shorts/31HVF6mR21c");
+    } else if (change == 3) {
+        window.open(
+            "https://www.youtube.com/@%D1%87%D0%BE%D1%80%D0%BD%D0%B8%D0%B9_%D0%B4%D0%B8%D0%BD%D0%BE333"
+        );
+    } else if (change == 4) {
+        window.open("https://www.youtube.com/@FGOT");
+    } else if (change == 5) {
+        window.open("https://www.youtube.com/watch?v=B9KRJF07S0k");
+    } else if (change == 6) {
+        window.open("https://www.youtube.com/watch?v=k2IEKgn-RTA");
+    }
+}
+function playSound(src) {
+    let audio = new Audio(src);
+    audio.play();
+}
+function moneyDown(num) {
+    playSound("./src/sound/money.mp3");
+    for (let i = 0; i < num; i++) {
+        let money = document.createElement("img");
+        money.src = "src/img/money.png";
+        money.className = "money";
+        money.draggable = false;
+        money.style.position = "absolute";
+        money.style.left = Math.random() * 100 + "vw";
+        money.style.top = "0";
+        let speed = Math.random() * 2 + 1;
+        money.style.animation = `down ${speed}s linear forwards`;
+        document.body.appendChild(money);
+        setTimeout(() => {
+            money.remove();
+        }, speed * 1000 + 500);
+    }
+}
+function experDown(num) {
+    playSound("./src/sound/experience.mp3");
+    for (let i = 0; i < num; i++) {
+        let money = document.createElement("img");
+        money.src = "src/img/experience.png";
+        money.className = "experience";
+        money.draggable = false;
+        money.style.position = "absolute";
+        money.style.left = Math.random() * 100 + "vw";
+        money.style.top = "0";
+        let speed = Math.random() * 2 + 1;
+        money.style.animation = `down ${speed}s linear forwards`;
+        document.body.appendChild(money);
+        setTimeout(() => {
+            money.remove();
+        }, speed * 1000 + 500);
+    }
+}
+
+const follower = document.getElementById("cursor");
+let percentX = 10;
+let percentY = 10;
+
+//add event listener and interval
+document.body.addEventListener(
+    "touchmove",
+    function (event) {
+        event.preventDefault();
+    },
+    { passive: false }
+);
+document.addEventListener("mousemove", (e) => {
+    percentX = (e.clientX / window.innerWidth) * 100;
+    percentY = (e.clientY / window.innerHeight) * 100;
+});
 
 animate();
+
+playSound("src/sound/calm.mp3");
+setInterval(() => {
+    playSound("src/sound/calm.mp3");
+}, 23000);
